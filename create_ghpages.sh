721ad716bb3e6a673c4e2fab2d3aada2fce75ef2
joelkim <kim.dohhyoung@gmail.com>
2017-12-14 10:10:24 +0900

    실행파일 삭제

--- create_ghpages.sh
+++ create_ghpages.sh
@@ -7,11 +7,16 @@ git push origin docs-korean
 
 git checkout -B gh-pages
 git rebase docs-korean
+
 touch .nojekyll
 echo '!Doc/build/' >> .gitignore
+
 cd Doc
 make html
 cd ..
+
+find . -name *.exe -delete
+
 git add . -A
 git commit -m "build"
 git push -f origin gh-pages
