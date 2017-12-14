7437cb067c4204fdbb1295f60eef8a2661446bde
joelkim <kim.dohhyoung@gmail.com>
2017-12-14 09:35:17 +0900

    automatic commit

--- create_ghpages.sh
+++ create_ghpages.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# current git branch must be `docs-korean`
+git add . -A
+git commit -m "automatic commit"
+git push origin docs-korean
 
 git checkout -B gh-pages
 git rebase docs-korean
